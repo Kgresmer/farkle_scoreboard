2d0b5d74aa4b27c50345f1303129930441da9322
Kevin Gresmer <kgresmer@gmail.com>
2022-03-07 14:34:27 -0600

    Prevent user from leaving scoreboard screen

--- lib/screens/scoreboard_screen.dart
+++ lib/screens/scoreboard_screen.dart
@@ -1,10 +1,18 @@
+import 'package:farkle_scoreboard/screens/fill_roster_screen.dart';
+import 'package:flutter/services.dart';
+
 import '../widgets/scoreboard_player_list.dart';
 import '../widgets/score_input.dart';
 import 'package:flutter/material.dart';
 
-class ScoreboardScreen extends StatelessWidget {
+class ScoreboardScreen extends StatefulWidget {
   static const routeName = '/scoreboard';
 
+  @override
+  _ScoreboardScreenState createState() => _ScoreboardScreenState();
+}
+
+class _ScoreboardScreenState extends State<ScoreboardScreen> {
   void openScoreInput(BuildContext context) {
     showModalBottomSheet(
         context: context,
@@ -17,46 +25,75 @@ class ScoreboardScreen extends StatelessWidget {
         });
   }
 
+  Future<bool> _backAPage(BuildContext context) async {
+    HapticFeedback.heavyImpact();
+    return (await showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit this game? The score will reset.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pushNamed(FillRosterScreen.routeName),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('build scoreboard screen');
 
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
-        title: Text('Farkle Scoreboard',
-            style: TextStyle(color: Theme.of(context).cardColor)),
-      ),
-      body: LayoutBuilder(builder: (ctx, constraints) {
-        return Column(
-          children: <Widget>[
-            Container(
-              height: constraints.maxHeight * .88,
-              child: ScoreboardPlayerList(),
+    return WillPopScope(
+        onWillPop: () => _backAPage(context),
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => _backAPage(context),
             ),
-            Container(
-              height: constraints.maxHeight * .12,
-              width: double.infinity,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                        height: constraints.maxHeight * .09,
-                        width: constraints.maxWidth - 25,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                                backgroundColor: Theme.of(context).shadowColor,
-                                textStyle: TextStyle(
-                                    fontSize: 30, fontWeight: FontWeight.bold)),
-                            onPressed: () => openScoreInput(context),
-                            child: Text('Begin scoring',
-                                style: TextStyle(
-                                    color: Theme.of(context).canvasColor)))),
-                  ]),
-            ),
-          ],
-        );
-      }),
-    );
+            title: Text('Farkle Scoreboard',
+                style: TextStyle(color: Theme.of(context).cardColor)),
+          ),
+          body: LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Container(
+                  height: constraints.maxHeight * .88,
+                  child: ScoreboardPlayerList(),
+                ),
+                Container(
+                  height: constraints.maxHeight * .12,
+                  width: double.infinity,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                            height: constraints.maxHeight * .09,
+                            width: constraints.maxWidth - 25,
+                            child: TextButton(
+                                style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        Theme.of(context).shadowColor,
+                                    textStyle: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold)),
+                                onPressed: () => openScoreInput(context),
+                                child: Text('Begin scoring',
+                                    style: TextStyle(
+                                        color:
+                                            Theme.of(context).canvasColor)))),
+                      ]),
+                ),
+              ],
+            );
+          }),
+        ));
   }
 }
