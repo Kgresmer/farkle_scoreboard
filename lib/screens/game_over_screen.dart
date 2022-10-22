6c2ebb709559f4d51d98693d953bbb15ef70185f
kgresmer <kgresmer@gmail.com>
2022-10-21 19:07:16 -0500

    Fixing back to home bug.

--- lib/screens/game_over_screen.dart
+++ lib/screens/game_over_screen.dart
@@ -47,7 +47,8 @@ class _GameOverScreenState extends State<GameOverScreen> {
             {ep.player.losses += 1},
           if (ep.player.highestRoll <
               rps
-                  .singleWhere((r) => r.player.id == ep.player.id)
+                  .singleWhere((r) => r.player.id == ep.player.id,
+                      orElse: () => new RosterPlayer(player: ep.player))
                   .player
                   .highestRoll)
             ep.player.highestRoll = rps
@@ -66,15 +67,24 @@ class _GameOverScreenState extends State<GameOverScreen> {
   String getGameOverBanner(String name) {
     int index = 1 + Random().nextInt(8 - 1);
     switch (index) {
-      case 1: return '$name played marvelously!';
-      case 2: return 'You crushed it $name!';
-      case 3: return 'You got pretty lucky there $name';
-      case 4: return '$name is making their mark on the map!';
-      case 5: return '$name is dancing with the stars!';
-      case 6: return '$name struck gold!';
-      case 7: return 'Nice risk management $name';
-      case 7: return 'Looking good $name!';
-      default: return 'You crushed it $name!';
+      case 1:
+        return '$name played marvelously!';
+      case 2:
+        return 'You crushed it $name!';
+      case 3:
+        return 'You got pretty lucky there $name';
+      case 4:
+        return '$name is making their mark on the map!';
+      case 5:
+        return '$name is dancing with the stars!';
+      case 6:
+        return '$name struck gold!';
+      case 7:
+        return 'Nice risk management $name';
+      case 7:
+        return 'Looking good $name!';
+      default:
+        return 'You crushed it $name!';
     }
   }
 
